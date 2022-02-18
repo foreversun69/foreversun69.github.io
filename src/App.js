597759961cc83739bbbeb091f1a034b10db3ce6a
blackcow33ps@gmx.de <blackcow33ps@gmx.de>
2022-02-18 09:33:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import { Routes, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { Overview } from './Overview';
+import { ManageBook } from './ManageBook';
+import { TimeLine } from './TimeLine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Navbar */}
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Buchverwaltung</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link ><Link to="/">Übersicht</Link></Nav.Link>
+              <Nav.Link ><Link to="/managebooks">Neues Buch</Link></Nav.Link>
+              <Nav.Link ><Link to="/timeline">TimeLine</Link></Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Main  */}
+      <Routes>
+        <Route exact path="/managebooks" element={<ManageBook />} />
+        <Route exact path="/timeline" element={<TimeLine />} />
+        <Route exact path="/" element={<Overview />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/ManageBook.jsx
@@ -0,0 +1,75 @@
+import React from "react"
+import { Form, Row, Button, Col, Container  } from 'react-bootstrap';
+
+export function ManageBook(){
+    return(
+/* <Form >
+<Container className="container-xs" >
+  <Row className="">
+    <Form.Group as={Col} controlId="formGridTitel">
+      <Form.Label>Titel</Form.Label>
+      <Form.Control type="text" placeholder="Titel vom Buch" />
+    </Form.Group>
+  </Row>
+
+  <Row className="">
+    <Form.Group as={Col} controlId="formGridAutor">
+      <Form.Label>Autor</Form.Label>
+      <Form.Control type="text" placeholder="Name des Autors" />
+    </Form.Group>
+  </Row>
+
+  <Row className="">
+    <Form.Group as={Col} controlId="formGridKommentar">
+      <Form.Label>Kommentar</Form.Label>
+      <Form.Control type="text" placeholder={"Kommentar"} />
+    </Form.Group>
+  </Row>
+
+  <Button className="" variant="primary" type="submit">
+    Submit
+  </Button>
+</Container>
+</Form> */
+
+
+<div className="container">
+  <div className="row justify-content-md-center m-2">
+    <div className="col col-lg-6">
+    <Form.Group as={Col} controlId="formGridTitel">
+      <Form.Label>Titel</Form.Label>
+      <Form.Control type="text" placeholder="Titel vom Buch" />
+    </Form.Group>
+    </div>
+  </div>
+  <div className="row justify-content-md-center m-2">
+    <div className="col col-lg-6">
+    <Form.Group as={Col} controlId="formGridAutor">
+      <Form.Label>Autor</Form.Label>
+      <Form.Control type="text" placeholder="Name des Autors" />
+    </Form.Group>
+    </div>
+  </div>
+  <div className="row justify-content-md-center m-2">
+    <div className="col col-lg-6">
+    <Form.Group as={Col} controlId="formGridKommentar">
+      <Form.Label>Kommentar</Form.Label>
+      <Form.Control type="text" placeholder={"Kommentar"} />
+    </Form.Group>
+    </div>
+  </div>
+  <div className="row justify-content-md-center pt-2 m-2">
+    <div className="col col-lg-6">
+    <Button className="" variant="primary" type="submit">
+        Submit
+    </Button>
+    </div>
+  </div>
+  
+</div>
+
+
+
+
+    )
+}
